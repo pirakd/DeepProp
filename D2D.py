c08d046ddfcc9cedbe2eab8a665ba627b3462fe9
pirakd <dpirak@gmail.com>
2022-01-20 19:59:48 +0200

    added a deconstructed variant for D2D

--- D2D.py
+++ D2D.py
@@ -22,6 +22,25 @@ def generate_D2D_features(source_features, terminal_featues):
     features = np.array(features).T
     return features
 
+def generate_D2D_features_2(source_features, terminal_featues):
+    """
+    :param source_features: list of experiments each of size [n_samples, n_sources, 2]
+    :param terminal_featues: list of experiments each of size [n_samples, n_terminals, 2]
+    :return: a numpy arrat of features of size [n_experiments, n_samples)
+    """
+
+    features = []
+    for exp_idx in range(len(source_features)):
+        source_sum = np.sum(source_features[exp_idx], axis=1)
+        terminal_sum = np.sum(terminal_featues[exp_idx], axis=1)
+
+        features.append([source_sum[:, 0], terminal_sum[:, 1], source_sum[:, 1], terminal_sum[:, 0]])
+
+    features = np.array(features)
+    features = np.array(features).reshape(-1, features.shape[-1])
+    features = np.array(features).T
+
+    return features
 
 def eval_D2D(train_features, test_features):
 
@@ -38,8 +57,32 @@ def eval_D2D(train_features, test_features):
     quantized_train_features = np.argsort(np.argsort(train_features, axis=1), axis=1)
     quantized_test_features = np.argsort(np.argsort(test_features, axis=1), axis=1)
 
-    clf = linear_model.LogisticRegression(solver='liblinear', penalty='l1', C=0.001)
+    clf = linear_model.LogisticRegression(solver='liblinear')
     probs = clf.fit(quantized_train_features, train_labels).predict_proba(quantized_test_features)
     acc = np.mean(np.argmax(probs, 1) == test_labels)
+    precision, recall, thresholds = precision_recall_curve(test_labels, probs[:,1])
+    mean_auc = auc(recall, precision)
+    return probs, precision, recall, mean_auc
 
-    return probs, test_labels, acc
+def eval_D2D_2(train_features, test_features):
+
+    inverse_idx = np.ravel([np.array([2, 3, 0, 1]) + 4*i  for i in range(int(train_features.shape[1]//4))])
+    inverse_train_features = train_features[: , inverse_idx]
+    inverse_test_features = test_features[: , inverse_idx]
+
+    train_labels, test_labels = np.zeros(train_features.shape[0] * 2), np.zeros(test_features.shape[0] * 2)
+    train_labels[:train_features.shape[0]] = 1
+    test_labels[:test_features.shape[0]] = 1
+
+    train_features = np.vstack([train_features, inverse_train_features])
+    test_features = np.vstack([test_features, inverse_test_features])
+    #
+    quantized_train_features = np.argsort(np.argsort(train_features, axis=1), axis=1)
+    quantized_test_features = np.argsort(np.argsort(test_features, axis=1), axis=1)
+
+    clf = linear_model.LogisticRegression(solver='liblinear')
+    probs = clf.fit(quantized_train_features, train_labels).predict_proba(quantized_test_features)
+    acc = np.mean(np.argmax(probs, 1) == test_labels)
+    precision, recall, thresholds = precision_recall_curve(test_labels, probs[:, 1])
+    mean_auc = auc(recall, precision)
+    return probs, precision, recall, mean_auc
