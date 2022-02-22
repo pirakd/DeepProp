18e88f46003e52abf4dee9b73c5f0d33842ff57f
pirakd <dpirak@gmail.com>
2022-02-22 12:58:01 +0200

    addition to last commit

--- deep_learning/deep_utils.py
+++ deep_learning/deep_utils.py
@@ -1,7 +1,7 @@
 from torch import nn
 from torch.autograd import Variable
 from torch.functional import F
-
+import torch
 
 def MaskedMean(x, mask, dim):
     mask = mask.int().unsqueeze(-1)
@@ -14,24 +14,19 @@ def MaskedSum(x, mask, dim):
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=0.8, reduction='sum'):
+    def __init__(self, gamma=0.8, reduction='mean'):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.reduction = reduction
-
+        self.BCE = torch.nn.BCEWithLogitsLoss(reduction='none')
     def forward(self, x, y):
-        y = y.view(-1, 1)
-
-        logpt = F.log_softmax(x, dim=1)
-        logpt = logpt.gather(1, y)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
-        loss = -1 * (1-pt)**self.gamma * logpt
+        logpt = -self.BCE(x, y)
+        pt = torch.exp(logpt)
+        loss = - (1 - pt) ** self.gamma * logpt
 
         if self.reduction == 'mean':
             return loss.mean()
         if self.reduction == 'sum':
             return loss.sum()
-
         else:
             assert 0, 'no such reduction for FocalLoss'
