928ffc0d8347ab95209dd1d90ffa7ef0c6635050
pirakd <dpirak@gmail.com>
2022-03-17 20:16:33 +0200

    some more refactoring

---
+++ scripts/compare_auc.py
@@ -0,0 +1,195 @@
+from os import path
+import sys
+sys.path.append(path.dirname(path.dirname(path.realpath(__file__))))
+from os import path, makedirs
+from deep_learning.data_loaders import ClassifierDataset, LightDataset
+from deep_learning.models import DeepPropClassifier, DeepProp
+from deep_learning.trainer import ClassifierTrainer
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+import torch
+from utils import read_data, generate_raw_propagation_scores, log_results, get_root_path, save_propagation_score,\
+    load_pickle, train_test_split, get_normalization_constants, get_loss_function, get_time
+from D2D import eval_D2D, eval_D2D_2, generate_D2D_features_from_propagation_scores
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import precision_recall_curve, auc
+from presets import experiments_20, experiments_50, experiments_all
+import json
+sources_filenmae_dict = {'drug': 'targets_drug',
+                         'AML': 'mutations_AML',
+                         'colon': 'mutations_colon',
+                         'ovary': 'mutations_ovary',
+                         'breast': 'mutations_breast'}
+terminals_filenmae_dict = {'drug': 'expressions_drug',
+                           'AML': 'gene_expressions_AML',
+                           'colon': 'gene_expressions_colon',
+                           'ovary': 'gene_expressions_ovary',
+                           'breast': 'gene_expressions_breast'}
+
+script_name = path.basename(__file__).split('.')[0]
+root_path = get_root_path()
+
+torch.set_default_dtype(torch.float32)
+output_folder = path.join(root_path, 'output', script_name, get_time())
+makedirs(output_folder)
+device = 'cpu'
+n_folds = 3
+n_experiments = 1
+input_name = 'drug'
+
+cmd_args = [int(arg) for arg in sys.argv[1:]]
+if len(cmd_args) == 2:
+    input_name = cmd_args[0]
+    n_experiments = cmd_args[2]
+    device = torch.device("cuda:{}".format(cmd_args[1]) if torch.cuda.is_available() else "cpu")
+
+if n_experiments == 'all' or n_experiments >= 100:
+    arg = experiments_all
+if n_experiments <= 30:
+    args = experiments_20
+else:
+    args = experiments_50
+
+args['data']['sources_filename'] = sources_filenmae_dict[input_name]
+args['data']['terminals_filename'] = terminals_filenmae_dict[input_name]
+args['data']['n_experiments'] = n_experiments
+
+
+# data read and filtering
+rng = np.random.RandomState(args['data']['random_seed'])
+
+network, directed_interactions, sources, terminals =\
+    read_data(args['data']['network_filename'], args['data']['directed_interactions_filename'],
+              args['data']['sources_filename'], args['data']['terminals_filename'],
+              args['data']['n_experiments'], args['data']['max_set_size'], rng)
+# merged_network = pandas.concat([directed_interactions, network.drop(directed_interactions.index & network.index)])
+directed_interactions_pairs_list = np.array(directed_interactions.index)
+genes_ids_to_keep = sorted(list(set([x for pair in directed_interactions_pairs_list for x in pair])))
+
+if args['data']['load_prop_scores']:
+    scores_file_path = path.join(root_path, 'input', 'propagation_scores', args['data']['prop_scores_filename'])
+    scores_dict = load_pickle(scores_file_path)
+    propagation_scores = scores_dict['propagation_scores']
+    row_id_to_idx, col_id_to_idx = scores_dict['row_id_to_idx'], scores_dict['col_id_to_idx']
+    normalization_constants_dict = scores_dict['normalization_constants']
+    sources_indexes = [[row_id_to_idx[gene_id] for gene_id in gene_set] for gene_set in sources.values()]
+    terminals_indexes = [[row_id_to_idx[gene_id] for gene_id in gene_set] for gene_set in terminals.values()]
+    pairs_indexes = [(col_id_to_idx[pair[0]], col_id_to_idx[pair[1]]) for pair in directed_interactions_pairs_list]
+    assert scores_dict['data_args']['random_seed'] == args['data']['random_seed'],\
+        'random seed of loaded data does not much current one'
+else:
+    propagation_scores, row_id_to_idx, col_id_to_idx =\
+        generate_raw_propagation_scores(network, sources, terminals, genes_ids_to_keep, args['propagation']['alpha'],
+                                        args['propagation']['n_iterations'], args['propagation']['eps'])
+    sources_indexes = [[row_id_to_idx[gene_id] for gene_id in gene_set] for gene_set in sources.values()]
+    terminals_indexes = [[row_id_to_idx[gene_id] for gene_id in gene_set] for gene_set in terminals.values()]
+    pairs_indexes = [(col_id_to_idx[pair[0]], col_id_to_idx[pair[1]]) for pair in directed_interactions_pairs_list]
+    normalization_constants_dict = get_normalization_constants(pairs_indexes, sources_indexes, terminals_indexes,
+                                                               propagation_scores)
+    if args['data']['save_prop_scores']:
+        save_propagation_score(propagation_scores, normalization_constants_dict, row_id_to_idx, col_id_to_idx,
+                               args['propagation'], args['data'], 'balanced_kpi_prop_scores')
+
+deep_probs_list, deep_labels_list, d2d_probs_list_2, d2d_labels_list_2, d2d_probs_list, d2d_labels_list = [],[],[],[],[],[]
+for fold in range(n_folds):
+    train_indexes, val_indexes, test_indexes = train_test_split(len(directed_interactions_pairs_list),
+                                                                args['train']['train_val_test_split'],
+                                                                random_state=rng)    # feature generation
+    d2d_train_indexes = np.concatenate([train_indexes, val_indexes])
+
+    # d2d evaluation
+
+    features, deconstructed_features = generate_D2D_features_from_propagation_scores(propagation_scores, pairs_indexes,
+                                                                                     sources_indexes, terminals_indexes)
+    d2d_probs, d2d_labels = eval_D2D(features[train_indexes], features[test_indexes])
+    d2d_probs_2, d2d_labels_2 = eval_D2D_2(deconstructed_features[train_indexes], deconstructed_features[test_indexes])
+
+    train_dataset = LightDataset(row_id_to_idx, col_id_to_idx, propagation_scores,
+                                 directed_interactions_pairs_list[train_indexes], sources, terminals,
+                                 normalization_constants_dict)
+    val_dataset = LightDataset(row_id_to_idx, col_id_to_idx, propagation_scores,
+                               directed_interactions_pairs_list[val_indexes], sources, terminals,
+                               normalization_constants_dict)
+    test_dataset = LightDataset(row_id_to_idx, col_id_to_idx, propagation_scores,
+                                directed_interactions_pairs_list[test_indexes], sources, terminals,
+                                normalization_constants_dict)
+
+    train_loader = DataLoader(train_dataset, batch_size=args['train']['train_batch_size'], shuffle=True,
+                              pin_memory=False)
+    val_loader = DataLoader(val_dataset, batch_size=args['train']['test_batch_size'], shuffle=False, pin_memory=False, )
+    test_loader = DataLoader(test_dataset, batch_size=args['train']['test_batch_size'], shuffle=False,
+                             pin_memory=False, )
+
+    # build models
+    deep_prop_model = DeepProp(args['model']['feature_extractor_layers'], args['model']['pulling_func'],
+                               args['model']['classifier_layers'], args['data']['n_experiments'],
+                               args['model']['exp_emb_size'])
+    model = DeepPropClassifier(deep_prop_model, args['data']['n_experiments'])
+
+    # train
+    intermediate_loss_type =  get_loss_function(args['train']['intermediate_loss_type'],
+                                                                focal_gamma=args['train']['focal_gamma'])
+    optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args['train']['learning_rate'])
+    trainer = ClassifierTrainer(args['train']['n_epochs'], criteria=nn.CrossEntropyLoss(),
+                                intermediate_criteria=intermediate_loss_type,
+                                intermediate_loss_weight=args['train']['intermediate_loss_weight'],
+                                optimizer=optimizer, eval_metric=None, eval_interval=args['train']['eval_interval'],
+                                device=device)
+
+    train_stats, best_model = trainer.train(train_loader=train_loader, eval_loader=val_loader, model=model,
+                                            max_evals_no_improvement=args['train']['max_evals_no_imp'])
+
+    deep_probs, deep_labels = trainer.eval(best_model, test_loader, output_probs=True)
+
+    deep_probs_list.append(deep_probs[:, 1])
+    deep_labels_list.append(deep_labels)
+    d2d_probs_list.append(d2d_probs[:, 1])
+    d2d_labels_list.append(d2d_labels)
+    d2d_probs_list_2.append(d2d_probs_2[:, 1])
+    d2d_labels_list_2.append(d2d_labels_2)
+
+deep_probs = np.hstack(deep_probs_list)
+deep_labels = np.hstack(deep_labels_list)
+d2d_probs = np.hstack(d2d_probs_list)
+d2d_labels = np.hstack(d2d_labels_list)
+d2d_probs_2 = np.hstack(d2d_probs_list_2)
+d2d_labels_2 = np.hstack(d2d_labels_list_2)
+
+deep_precision, deep_recall, deep_thresholds = precision_recall_curve(deep_labels, deep_probs)
+deep_auc = auc(deep_recall, deep_precision)
+
+d2d_precision, d2d_recall, d2d_thresholds = precision_recall_curve(d2d_labels, d2d_probs)
+d2d_auc = auc(d2d_recall, d2d_precision)
+
+d2d_precision_2, d2d_recall_2, d2d_thresholds_2 = precision_recall_curve(d2d_labels_2, d2d_probs_2)
+d2d_auc_2 = auc(d2d_recall_2, d2d_precision_2)
+
+
+plt.plot([0, 1], [0.5, 0.5], '--', color=(0.8, 0.8, 0.8), label='random')
+plt.plot(deep_recall, deep_precision, 'o--', marker='o', label='DeepSetPropagation (%0.2f)' % deep_auc, lw=2, markersize=3)  # , markeredgecolor = 'dimgrey')
+plt.plot(d2d_recall, d2d_precision, 'o--', marker='o', label='D2D (%0.2f)' % d2d_auc, lw=2, markersize=3)  # , markeredgecolor = 'dimgrey')
+plt.plot(d2d_recall_2, d2d_precision_2, 'o--', marker='o', label='D2D deconstructed (%0.2f)' % d2d_auc_2, lw=2, markersize=3)  # , markeredgecolor = 'dimgrey')
+
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.legend(loc="lower right")
+plt.title('{}, {}, {} sources, {} folds'.format(args['data']['directed_interactions_filename'],
+                                                args['data']['sources_filename'].split('_')[-1],
+                                                args['data']['n_experiments'], n_folds))
+
+params = {'legend.fontsize': 8,
+          'figure.figsize': (4.6, 2.9),
+          'axes.labelsize': 8,
+          'axes.titlesize': 8,
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8}
+
+plt.rcParams.update(params)
+plt.savefig(path.join(output_folder, 'auc_curve'))
+
+with open(path.join(output_folder, 'args'), 'w') as f:
+    json.dump(args, f, indent=4, separators=(',', ': '))
--- scripts/parameter_search.py
+++ scripts/parameter_search.py
@@ -2,17 +2,18 @@ from os import path
 import sys
 sys.path.append(path.dirname(path.dirname(path.realpath(__file__))))
 from os import makedirs
-from deep_learning.data_loaders import ClassifierDataset
+from deep_learning.data_loaders import LightDataset
 from deep_learning.models import DeepPropClassifier, DeepProp
 from deep_learning.trainer import ClassifierTrainer
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.optim import Adam
-from utils import read_data, generate_feature_columns, normalize_features, log_results,\
-    get_root_path, train_test_split, get_loss_function
+from utils import read_data, generate_raw_propagation_scores, log_results, get_root_path, save_propagation_score,\
+    load_pickle, train_test_split, get_normalization_constants, get_loss_function
+from sklearn.metrics import precision_recall_curve, auc
 import torch
-import random
 import numpy as np
+import random
 
 device = torch.device("cuda".format() if torch.cuda.is_available() else "cpu")
 output_folder = 'output'
@@ -29,30 +30,36 @@ TERMINALS_FILENAME = path.join(input_file, 'priors', "drug_expressions.txt")
 
 args = {
     'data':
-        {'n_experiments': 2,
-         'train_test_split': 0.8,
-         'dataset_type': 'balanced_kpi',
-         'random_seed':0},
+        {'n_experiments': 50,
+         'max_set_size': 400,
+         'network_filename': 'H_sapiens.net',
+         'directed_interactions_filename': 'KPI_dataset',
+         'sources_filename': 'drug_targets.txt',
+         'terminals_filename': 'drug_expressions.txt',
+         'load_prop_scores': True,
+         'save_prop_scores': False,
+         'prop_scores_filename': 'balanced_kpi_prop_scores',
+         'random_seed': 0},
     'propagation':
         {'alpha': 0.8,
          'eps': 1e-6,
          'n_iterations': 200},
     'model':
-        {'feature_extractor_layers': [64, 32, 16],
-         'classifier_layers': [32, 16, 8],
+        {'feature_extractor_layers': [64, 32],
+         'classifier_layers': [128, 64],
          'pulling_func': 'mean',
-         'exp_emb_size': 4},
+         'exp_emb_size': 12},
     'train':
-        {'intermediate_loss_weight': 0.95,
-         'intermediate_loss_type' : 'BCE',
-         'focal_gamma': 0.5,
-         'train_val_test_split': [0.8, 0.2, 0],
-         'test_batch_size': 32,
+        {'intermediate_loss_weight': 0.5,
+         'intermediate_loss_type': 'BCE',
+         'focal_gamma': 1,
+         'train_val_test_split': [0.66, 0.14, 0.2], # sum([train, val, test])=1
          'train_batch_size': 16,
-         'n_epochs': 100,
-         'eval_interval': 5,
-         'learning_rate': 1e-3,
-         'max_evals_no_imp': 10 },}
+         'test_batch_size': 32,
+         'n_epochs':2000 ,
+         'eval_interval': 2,
+         'learning_rate': 5e-4,
+         'max_evals_no_imp': 25}}
 
 device = torch.device("cuda".format() if torch.cuda.is_available() else "cpu")
 cmd_args = [int(arg) for arg in sys.argv[1:]]
@@ -67,27 +74,39 @@ network, directed_interactions, sources, terminals =\
     read_data(NETWORK_FILENAME, DIRECTED_INTERACTIONS_FILENAME, SOURCES_FILENAME, TERMINALS_FILENAME,
               args['data']['n_experiments'], args['data']['max_set_size'], rng)
 
-directed_interactions_pairs_list = tuple(directed_interactions.index)
-pairs_to_index = {pair: p for p, pair in enumerate(network.index)}
-directed_interaction_set = set(directed_interactions_pairs_list)
-labeled_pairs_to_index = {pair: idx for pair, idx in pairs_to_index.items() if pair in directed_interaction_set}
-indexes_to_keep = list(labeled_pairs_to_index.values())
 
-# feature generation
-source_features, terminal_features = generate_feature_columns(network, sources, terminals, indexes_to_keep,
-                                                              args['propagation']['alpha'], args['propagation']['n_iterations'],
-                                                              args['propagation']['eps'])
+directed_interactions_pairs_list = np.array(directed_interactions.index)
+genes_ids_to_keep = sorted(list(set([x for pair in directed_interactions_pairs_list for x in pair])))
 
-#pro processing
-source_features, terminal_features = normalize_features(source_features, terminal_features)
+if args['data']['load_prop_scores']:
+    scores_file_path = path.join(root_path, 'input', 'propagation_scores', args['data']['scores_filename'])
+    scores_dict = load_pickle(scores_file_path)
+    propagation_scores = scores_dict['propagation_scores']
+    row_id_to_idx, col_id_to_idx = scores_dict['row_id_to_idx'], scores_dict['col_id_to_idx']
+    normalization_constants_dict = scores_dict['normalization_constants']
+    sources_indexes = [[row_id_to_idx[id] for id in set] for set in sources.values()]
+    terminals_indexes = [[row_id_to_idx[id] for id in set] for set in terminals.values()]
+    pairs_indexes = [(col_id_to_idx[pair[0]], col_id_to_idx[pair[1]]) for pair in directed_interactions_pairs_list]
+    assert scores_dict['data_args']['random_seed'] == args['data']['random_seed'], 'random seed of loaded data does not much current one'
+else:
+    propagation_scores, row_id_to_idx, col_id_to_idx = generate_raw_propagation_scores(network, sources, terminals, genes_ids_to_keep,
+                                                                  args['propagation']['alpha'], args['propagation']['n_iterations'],
+                                                                  args['propagation']['eps'])
+    sources_indexes = [[row_id_to_idx[id] for id in set] for set in sources.values()]
+    terminals_indexes = [[row_id_to_idx[id] for id in set] for set in terminals.values()]
+    pairs_indexes = [(col_id_to_idx[pair[0]], col_id_to_idx[pair[1]]) for pair in directed_interactions_pairs_list]
+    normalization_constants_dict = get_normalization_constants(pairs_indexes, sources_indexes, terminals_indexes,
+                                                               propagation_scores)
+    if args['data']['save_prop_scores']:
+        save_propagation_score(propagation_scores, normalization_constants_dict, row_id_to_idx, col_id_to_idx,
+                               args['propagation'], args['data'], 'balanced_kpi_prop_scores')
 
-train_indexes, val_indexes, _ = train_test_split(len(indexes_to_keep), args['train']['train_val_test_split'])
 
-train_source_features = [x[train_indexes] for x in source_features]
-test_source_features = [x[val_indexes] for x in source_features]
-train_terminal_features = [x[train_indexes] for x in terminal_features]
-test_terminal_features = [x[val_indexes] for x in terminal_features]
-
+train_indexes, val_indexes, test_indexes = train_test_split(len(directed_interactions_pairs_list), args['train']['train_val_test_split'],
+                                                            random_state=rng)
+train_dataset = LightDataset(row_id_to_idx, col_id_to_idx, propagation_scores, directed_interactions_pairs_list[train_indexes], sources, terminals, normalization_constants_dict)
+val_dataset = LightDataset(row_id_to_idx, col_id_to_idx, propagation_scores, directed_interactions_pairs_list[val_indexes], sources, terminals, normalization_constants_dict)
+test_dataset = LightDataset(row_id_to_idx, col_id_to_idx, propagation_scores, directed_interactions_pairs_list[test_indexes], sources, terminals, normalization_constants_dict)
 
 feature_extractor_layers = [[128,64], [64, 32, 16], [64,32], [16, 8]]
 classifier_layers = [[128, 64, 32], [128, 64], [32, 16, 8],  [32, 16], [64, 32, 16]]
@@ -97,6 +116,9 @@ train_batch_size = [16, 32, 64]
 exp_embedding_size = [4, 8, 12, 16]
 intermediate_loss_type_list = ['BCE', 'FOCAL']
 focal_gamma = [0.5, 1, 2, 3]
+val_loader = DataLoader(val_dataset, batch_size=args['train']['test_batch_size'], shuffle=False, pin_memory=True, )
+test_loader = DataLoader(test_dataset, batch_size=args['train']['test_batch_size'], shuffle=False, pin_memory=True, )
+
 while True:
     args['model']['feature_extractor_layers'] = random.choice(feature_extractor_layers)
     args['model']['classifier_layers'] = random.choice(classifier_layers)
@@ -108,11 +130,9 @@ while True:
     args['train']['intermediate_loss_type'] = np.random.choice(intermediate_loss_type_list, p=[0.2, 0.8])
     intermediate_loss_type =  get_loss_function(args['train']['intermediate_loss_type'],
                                                                 focal_gamma=args['train']['focal_gamma'])
-    train_dataset = ClassifierDataset(train_source_features, train_terminal_features)
-    test_dataset = ClassifierDataset(test_source_features, test_terminal_features)
+
     train_loader = DataLoader(train_dataset, batch_size=args['train']['train_batch_size'], shuffle=True,
                               pin_memory=True)
-    eval_loader = DataLoader(test_dataset, batch_size=args['train']['test_batch_size'], shuffle=False, pin_memory=True)
 
     deep_prop_model = DeepProp(args['model']['feature_extractor_layers'], args['model']['pulling_func'],
                                args['model']['classifier_layers'], args['data']['n_experiments'],
@@ -127,10 +147,19 @@ while True:
                                 intermediate_loss_weight=args['train']['intermediate_loss_weight'], device=device,
                                 optimizer=optimizer, eval_metric=None, eval_interval=args['train']['eval_interval'])
 
+    try:
+        train_stats, best_model =\
+            trainer.train(train_loader=train_loader, eval_loader=val_loader, model=model, max_evals_no_improvement=args['train']['max_evals_no_imp'])
+    except Exception as e:
+        print(e)
+        continue
 
-    train_stats, best_model =\
-        trainer.train(train_loader=train_loader, eval_loader=eval_loader, model=model, max_evals_no_improvement=args['train']['max_evals_no_imp'])
+    test_loss, test_intermediate_loss, test_classifier_loss, test_acc, test_auc, precision, recall =\
+        trainer.eval(best_model, test_loader)
 
-    results_dict = {'args': args, 'train_stats': train_stats}
+    test_stats = {'test_loss': test_loss, 'best_acc': test_intermediate_loss, 'best_auc': test_auc,
+                   'test_intermediate_loss': test_intermediate_loss, 'test_classifier_loss':test_classifier_loss}
+    print('Test PR-AUC: {:.2f}'.format(test_auc))
+    results_dict = {'args': args, 'train_stats': train_stats, 'test_stats':test_stats}
 
     log_results(results_dict, output_file_path)
