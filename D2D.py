41d65a8464b1e74afbf8d850ae5783c294547282
pirakd <dpirak@gmail.com>
2022-03-22 15:11:22 +0200

    added method for generating D2D features from raw propagation scores

--- D2D.py
+++ D2D.py
@@ -3,6 +3,30 @@ from sklearn import linear_model
 from sklearn.metrics import precision_recall_curve, auc
 
 
+def generate_D2D_features_from_propagation_scores(propagation_scores, pairs_indexes, source_indexes, terminal_indexes):
+    """
+    :param source_features: list of experiments each of size [n_samples, n_sources, 2]
+    :param terminal_featues: list of experiments each of size [n_samples, n_terminals, 2]
+    :return: a numpy array of features of size [n_experiments, n_samples)
+    """
+
+    gene1, gene2 = map(lambda x: x, zip(*[[gene for gene in pair] for pair in pairs_indexes]))
+    features = []
+    deconstructed_features = []
+    for exp_idx in range(len(source_indexes)):
+        source_feature_1 = np.sum(propagation_scores[source_indexes[exp_idx], :][:, gene1],axis=0)
+        source_feature_2 = np.sum(propagation_scores[source_indexes[exp_idx],:][:,gene2],axis=0)
+        terminal_feature_1 = np.sum(propagation_scores[terminal_indexes[exp_idx],:][:,gene1],axis=0)
+        terminal_feature_2 = np.sum(propagation_scores[terminal_indexes[exp_idx],:][:,gene2],axis=0)
+
+        numerator = source_feature_1* terminal_feature_2
+        denominator = source_feature_2 * terminal_feature_1
+        features.append(numerator/denominator)
+        deconstructed_features.append(np.array([source_feature_1, terminal_feature_1, source_feature_2,  terminal_feature_2 ]))
+    features = np.array(features).T
+    deconstructed_features = np.array(np.concatenate(deconstructed_features,axis=0)).T
+    return features, deconstructed_features
+
 def generate_D2D_features(source_features, terminal_featues):
     """
     :param source_features: list of experiments each of size [n_samples, n_sources, 2]
@@ -26,7 +50,7 @@ def generate_D2D_features_2(source_features, terminal_featues):
     """
     :param source_features: list of experiments each of size [n_samples, n_sources, 2]
     :param terminal_featues: list of experiments each of size [n_samples, n_terminals, 2]
-    :return: a numpy arrat of features of size [n_experiments, n_samples)
+    :return: a numpy array of features of size [n_experiments, n_samples)
     """
 
     features = []
@@ -57,7 +81,7 @@ def eval_D2D(train_features, test_features):
     quantized_train_features = np.argsort(np.argsort(train_features, axis=1), axis=1)
     quantized_test_features = np.argsort(np.argsort(test_features, axis=1), axis=1)
 
-    clf = linear_model.LogisticRegression(solver='liblinear', penalty='l1', C=0.001)
+    clf = linear_model.LogisticRegression(solver='liblinear',  penalty='l1', C=0.0001)
     probs = clf.fit(quantized_train_features, train_labels).predict_proba(quantized_test_features)
     acc = np.mean(np.argmax(probs, 1) == test_labels)
     precision, recall, thresholds = precision_recall_curve(test_labels, probs[:, 1])
@@ -66,7 +90,7 @@ def eval_D2D(train_features, test_features):
 
 def eval_D2D_2(train_features, test_features):
 
-    inverse_idx = np.ravel([np.array([2, 3, 0, 1]) + 4*i  for i in range(int(train_features.shape[1]//4))])
+    inverse_idx = np.ravel([np.array([2, 3, 0, 1]) + 4*i for i in range(int(train_features.shape[1]//4))])
     inverse_train_features = train_features[: , inverse_idx]
     inverse_test_features = test_features[: , inverse_idx]
 
@@ -80,7 +104,7 @@ def eval_D2D_2(train_features, test_features):
     quantized_train_features = np.argsort(np.argsort(train_features, axis=1), axis=1)
     quantized_test_features = np.argsort(np.argsort(test_features, axis=1), axis=1)
 
-    clf = linear_model.LogisticRegression(solver='liblinear', penalty='l1', C=0.001)
+    clf = linear_model.LogisticRegression(solver='liblinear', penalty='l1', C=0.0001)
     probs = clf.fit(quantized_train_features, train_labels).predict_proba(quantized_test_features)
     acc = np.mean(np.argmax(probs, 1) == test_labels)
     precision, recall, thresholds = precision_recall_curve(test_labels, probs[:, 1])
